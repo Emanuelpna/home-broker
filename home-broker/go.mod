672b999082773bd02c59d48f46f61243d4ef7ae1
Emanuel Andrade <emanuelpna@yahoo.com.br>
2023-06-23 14:31:38 -0300

    day 1 - created base go code

---
+++ home-broker/go.mod
@@ -0,0 +1,5 @@
+module github.com/Emanuelpna/home-broker
+
+go 1.20
+
+require github.com/google/uuid v1.3.0
---
+++ home-broker/internal/market/entity/book.go
@@ -0,0 +1,111 @@
+package entity
+
+import (
+	"container/heap"
+	"sync"
+)
+
+type Book struct {
+	Orders              []*Order
+	Transactions        []*Transaction
+	OrdersChannelInput  chan *Order
+	OrdersChannelOutput chan *Order
+	WaitGroup           *sync.WaitGroup
+}
+
+func NewBook(ordersChannelInput chan *Order, ordersChannelOutput chan *Order, waitGroup *sync.WaitGroup) *Book {
+	return &Book{
+		Orders:              []*Order{},
+		Transactions:        []*Transaction{},
+		OrdersChannelInput:  ordersChannelInput,
+		OrdersChannelOutput: ordersChannelOutput,
+		WaitGroup:           waitGroup,
+	}
+}
+
+func (b *Book) Trade() {
+	buyOrders := NewOrderQueue()
+	sellOrders := NewOrderQueue()
+
+	heap.Init(buyOrders)
+	heap.Init(sellOrders)
+
+	for order := range b.OrdersChannelInput {
+		// É um pedido de COMPRA de ação
+		if order.OrderType == "BUY" {
+			buyOrders.Push(order)
+
+			// Existem pedidos de VENDA e tem o mesmo preço que o pedido de COMPRA
+			if sellOrders.Len() > 0 && sellOrders.Orders[0].Price <= order.Price {
+				sellOrder := sellOrders.Pop().(*Order)
+
+				// Existem cotas sobrando para serem transacionadas
+				if sellOrder.PendingShares > 0 {
+					transaction := NewTransaction(sellOrder, order, order.Shares, sellOrder.Price)
+					b.AddTransaction(transaction, b.WaitGroup)
+
+					sellOrder.Transactions = append(sellOrder.Transactions, transaction)
+					order.Transactions = append(order.Transactions, transaction)
+
+					// Envia os pedidos para o Canal de Output
+					b.OrdersChannelOutput <- sellOrder
+					b.OrdersChannelOutput <- order
+
+					if sellOrder.PendingShares > 0 {
+						sellOrders.Push(sellOrder)
+					}
+				}
+			}
+		} else if order.OrderType == "SELL" {
+			sellOrders.Push(order)
+
+			// Existem pedidos de VENDA e tem o mesmo preço que o pedido de COMPRA
+			if buyOrders.Len() > 0 && buyOrders.Orders[0].Price >= order.Price {
+				buyOrder := buyOrders.Pop().(*Order)
+
+				// Existem cotas sobrando para serem transacionadas
+				if buyOrder.PendingShares > 0 {
+					transaction := NewTransaction(order, buyOrder, order.Shares, buyOrder.Price)
+					b.AddTransaction(transaction, b.WaitGroup)
+
+					buyOrder.Transactions = append(buyOrder.Transactions, transaction)
+					order.Transactions = append(order.Transactions, transaction)
+
+					// Envia os pedidos para o Canal de Output
+					b.OrdersChannelOutput <- buyOrder
+					b.OrdersChannelOutput <- order
+
+					// Manda de volta pra fila se ainda continua tem cotas sobrando
+					if buyOrder.PendingShares > 0 {
+						buyOrders.Push(buyOrder)
+					}
+				}
+			}
+		}
+	}
+}
+
+func (b *Book) AddTransaction(transaction *Transaction, waitGroup *sync.WaitGroup) {
+	defer waitGroup.Done()
+
+	sellingShares := transaction.SellingOrder.PendingShares
+	buyingShares := transaction.BuyingOrder.PendingShares
+
+	minShares := sellingShares
+	if buyingShares < minShares {
+		minShares = buyingShares
+	}
+
+	transaction.SellingOrder.Investor.UpdateAssetPosition(transaction.SellingOrder.Asset.ID, -minShares)
+	transaction.AddBuyingOrderPendingShares(-minShares)
+
+	transaction.BuyingOrder.Investor.UpdateAssetPosition(transaction.BuyingOrder.Asset.ID, minShares)
+	transaction.AddSellingOrderPendingShares(-minShares)
+
+	transaction.CalculateTotal(transaction.Shares, transaction.BuyingOrder.Price)
+
+	transaction.CloseBuyingOrder()
+	transaction.CloseSellingOrder()
+
+	b.Transactions = append(b.Transactions, transaction)
+}
